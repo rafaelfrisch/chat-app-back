f1d20de986dcf558eab076816e580c5e5f10c0d5
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-10-04 18:59:59 -0300

    Project template

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import cors from 'cors';
+import logger from 'morgan';
+import express from 'express';
+import compression from 'compression';
+import { logErrors, clientError, serverError } from './errorHandlers';
+import './connect';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors('*'));
+app.use(compression());
+
+
+app.use(logErrors);
+app.use(clientError);
+app.use(serverError);
+
+export default app;
