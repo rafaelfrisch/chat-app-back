bba7076830610aa005ee1f10e77dafb496697134
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-10-05 16:22:38 -0300

    Create conversation

--- src/controllers/conversation.js
+++ src/controllers/conversation.js
@@ -2,8 +2,24 @@ import * as models from '../models'
 
 export const createConversation = async (request, response) => {
     try {
-        console.log('entrou')
+        const user = request.user
+
+        if(!user)
+            throw new Error()
+        
+        // check if users exists
+        const users = await models.User.find({_id : { $in: request.body.users } })
+
+        if(!users)
+            throw new Error()
+
+        const conversation = new models.Conversation({ users: request.body.users })
+       
+        await conversation.save()
+
+        response.status(201).send(users)
+
     } catch (error) {
-        console.log('erro')
+        response.status(400).send(error)
     }
 }
