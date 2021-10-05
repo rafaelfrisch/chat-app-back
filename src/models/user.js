1e9621c15e60eaf6bce0d47bfd2815f52888134e
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-10-04 22:48:45 -0300

    Auth token

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+import { jwtPrivateKey } from '../settings';
+
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -21,8 +24,24 @@ const userSchema = new mongoose.Schema({
     created: { 
         type: Date, 
         default: Date.now 
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user.id.toString() }, jwtPrivateKey)
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
 
 userSchema.pre('save', async function (next) {
     const user = this
--- src/settings.js
+++ src/settings.js
@@ -3,3 +3,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const connectionString = process.env.CONNECTION_STRING.trim();
+export const jwtPrivateKey = process.env.JWT_PRIVATE_KEY.trim();
