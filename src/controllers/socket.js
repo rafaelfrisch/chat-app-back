83963e6e6003d55009fa9b4f0a8ce98fea7861c2
rafaelfrisch <rafaelcassolfrisch@gmail.com>
2021-12-11 04:03:41 -0300

    Remove console.log

--- src/controllers/socket.js
+++ src/controllers/socket.js
@@ -3,7 +3,6 @@ import * as constants from '../settings'
 const jwt = require('jsonwebtoken')
 
 const socketController = async (token, callback) => {
-    console.log(token);
     const decoded = jwt.verify(token, constants.jwtPrivateKey)
     const user = await models.User.findOne({ _id: decoded._id, 'tokens.token': token})
     const userConversations = await models.Conversation.find({ 'users': user._id }).populate('users', ['username', '_id'])
